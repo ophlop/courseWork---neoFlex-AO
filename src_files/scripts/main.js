167a06b9ee6b41ac5f35efd45ff123d0c25e3163
Astashin Oleg <astashinoleg@gmail.com>
2023-01-10 11:11:35 +0300

    - update js code

--- src_files/scripts/main.js
+++ src_files/scripts/main.js
@@ -1,84 +1,39 @@
 // Currency API script part begin
+let coine = "USD";
+let coineTwo = "RUB";
+let json;
 
-let showLeftRowInfo = false;
-let currencyPairsPrice = ["USD", "CNY", "CHF", "EUR", "JPY", "TRY"];
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key":
+      "34eb46cf27msh7166df608a6238cp161b5ejsn0f643a99bbf7",
+    "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
+  },
+};
+let currencyPairsPrice = ["USD", "EUR", "CNY", "CHF", "JPY", "TRY"];
 
-function createCourses(currencyPairsPrice) {
-  showLeftRowInfo = true;
-  let loader = document.querySelector(".loader");
-  loader.classList.add(".hide-loader");
-  let list = document.querySelector(".left-row__course-info");
-  for (let i = 0; i < currencyPairsPrice.length; i++) {
-    list.insertAdjacentHTML(
-      "beforeend",
-      `
-          <div class="left-row__course-card">
-            <span class="course-card__currencies">${currencyPairsPrice[i]}:</span>
-            <span class="course-card__price-course">${currencyPairsPrice[i].course}</span>
-          </div>
-      `
-    );
-  }
-}
-
-function updateCourses(currencyPairsPrice) {
-  console.log("update!");
-  let values = document.querySelectorAll(".course-card__price-course");
-  for (let i = 0; i < currencyPairsPrice.length; i++) {
-    values[i].innerHTML = currencyPairsPrice[i].course;
-  }
-}
-
-function showActualCourses(currencyPairsPrice = [{ from: "USD", to: "RUB" }]) {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "34eb46cf27msh7166df608a6238cp161b5ejsn0f643a99bbf7",
-      "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
-    },
-  };
-
-  let requests = currencyPairsPrice.map((el) =>
-    fetch(
-      `https://currency-exchange.p.rapidapi.com/exchange?from=${el}&to=RUB&q=1.0`,
-      options
-    )
-      .then((response) => {
-        if (!response.ok) {
-          console.log("response1", response);
-          throw new Error("Fetch error");
-        }
-        console.log("response2", response);
-        return response.json();
-      })
-      .then((response) => {
-        el.course = response.toFixed(2);
-        console.log("response3", response.json());
-      })
-  );
-  return requests;
-  Promise.all(requests)
-    .then(() => {
-      if (!showLeftRowInfo) {
-        createCourses(currencyPairsPrice);
-        console.log("CC", createCourses(currencyPairsPrice));
-      } else {
-        updateCourses(currencyPairsPrice);
-        console.log("else CC", updateCourses(currencyPairsPrice));
-      }
+let renderDIVParent = document.querySelector(".left-row__course-info");
+for (let i = 0; i < currencyPairsPrice.length; i++) {
+  let renderDIVChild = document.createElement('div')
+  let span1 = document.createElement("span");
+  let span2 = document.createElement("span");
+  renderDIVChild.classList.add("left-row__course-card")
+  span1.classList.add("course-card__currencies");
+  span2.classList.add("course-card__price-course");
+  span1.innerHTML = `${currencyPairsPrice[i]}:`;
+  span2.innerHTML = `${fetch(
+    `https://currency-exchange.p.rapidapi.com/exchange?from=${currencyPairsPrice[i]}&to=${coineTwo}&q=1.0`,
+    options
+  )
+    .then((response) => {
+      return response.json();
     })
-    .catch((err) => {
-      console.log(`${err}`);
-    });
+    .then((data) => {
+      span2.innerHTML = `${data.toFixed(2)}`;
+    })}`;
+  renderDIVChild.appendChild(span1);
+  renderDIVChild.appendChild(span2);
+  renderDIVParent.appendChild(renderDIVChild)
 }
-
-showActualCourses(currencyPairsPrice);
-setInterval(() => {
-  showActualCourses(currencyPairsPrice);
-}, 900000);
-
 // Currency API script part end
-
-// News Block script part start
-
-// News Block script part end
